24714e8fa04fd0d6b9667e6d0c2b5990f78087f4
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-02-18 22:20:27 +0700

    Commit main branch

--- MergeTest/Program.cs
+++ MergeTest/Program.cs
@@ -1,1 +1,4 @@
-﻿// Đây là nhánh "test-branch"
+﻿Console.Write("Nhập tên: ");
+string name;
+name = Console.ReadLine() ?? throw new Exception("Vui lòng nhập tên");
+Console.WriteLine($"Hello world, {name}");
